b0980e32396451fb0e94e5c3e2a7f08de4dcb132
Grilindor <marcor91@hotmail.fr>
2024-05-09 15:48:35 +0200

    test

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
 import hidden_4
+
 if __name__ == "__main__":
-    for i in range(hidden_4):
-        if i[:2] != "__":
-            print("{}".format(i))
+    names = dir(hidden_4)
+    sorted_name = sorted(name for name in names if not name.startswith("__"))
+    for name in sorted_name:
+        print(name)
+
+# import hidden_4
+# if __name__ == "__main__":
+#    for i in range(hidden_4):
+#        if i[:2] != "__":
+#            print("{}".format(i))
