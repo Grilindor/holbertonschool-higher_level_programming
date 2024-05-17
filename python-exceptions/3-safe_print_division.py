da5a5ecab178e46f1e7e51ff202a9fb0e7ce6606
Grilindor <marcor91@hotmail.fr>
2024-05-17 15:59:44 +0200

    task3, try different organistion

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,8 +4,10 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a/b
-        return result
     except ZeroDivisionError:
-        return "None"
+        result = None
     finally:
-        print("Inside result: {}".format(result))
+        if result is not None:
+            print("Inside result: {}".format(result))
+        else:
+            print("Inside result: None")
