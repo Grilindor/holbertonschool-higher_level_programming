e1492d21bfd3a936c24c15dba397de052ad15257
Grilindor <marcor91@hotmail.fr>
2024-05-18 16:35:55 +0200

    task5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -31,8 +31,8 @@ class Square:
 
     def my_print(self):
         if self.__size == 0:
-            print("\n")
+            print("")
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end='')
-            print("\n")
+            print("")
