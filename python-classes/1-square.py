246a479fc448aa1cb6ddf6f05474f91f1fdc7189
Grilindor <marcor91@hotmail.fr>
2024-05-18 11:12:13 +0200

    task1, revue the comments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 """
 In this class, we have a private instance attribute
-__size
+_Square__size
 that represents the size of the square. The
 __init__
 method is used for instantiation with the size provided. We also have a
-property size
-to access the size attribute and a setter method to update the size if needed.
-The area
-method calculates and returns the area of the square.
+property : size
 """
 
 
 class Square:
     """ a defined square"""
     def __init__(self, size):
+        # Initialize the Square class with a given size
         self._Square__size = size
-
-
