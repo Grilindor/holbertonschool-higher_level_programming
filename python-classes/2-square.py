fea28b6bead1b877b66b1af5869e75910023298c
Grilindor <marcor91@hotmail.fr>
2024-05-18 13:54:06 +0200

    task2,  a class Square that defines a square by: (based on 1-square.py)

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+class Square:
+    """ a defined square"""
+    def __init__(self, size):
+        # Initialize the Square class with a given size
+        self._Square__size = size
+
+    def __init__(self, size=0):
+        self._Square__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
