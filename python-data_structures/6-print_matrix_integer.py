800562a0b931dfaf0d10127dc1534d3ed583af90
Grilindor <marcor91@hotmail.fr>
2024-05-19 15:33:35 +0200

    task6, pycodestyle

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -10,11 +10,11 @@ Returns:
 The matrix printed
 """
 
-def print_matrix_integer(matrix=[[]]):
-        for row in range(len(matrix)):
-                for column in range(len(matrix[row])):
-                        print("{:d}".format(matrix[row][column]), end="")
-                        if column != len(matrix[0]) - 1:
-                                print(" ", end="")
-                print("")
 
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][column]), end="")
+            if column != len(matrix[0]) - 1:
+                print(" ", end="")
+        print("")
