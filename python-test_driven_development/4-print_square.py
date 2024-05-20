3d3cd45b18cc138462aaf03ed99416fc8fe87c9d
Grilindor <marcor91@hotmail.fr>
2024-05-20 19:43:53 +0200

    task3,  function that prints a square with the character #, and some test

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+function that prints a square with the character #.
+"""
+
+
+def print_square(size):
+    # size be an integer
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    # size not float, no less than 0
+    if size < 0:
+        if isinstance(size, float):
+            raise TypeError("size must be an integer")
+        raise ValueError("size must be >= 0")
+
+    # print the square
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print("")
