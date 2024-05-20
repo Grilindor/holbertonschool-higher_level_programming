82e4ccc7fa4e8ae3b8c27a6b408a88c5632d8ec0
Grilindor <marcor91@hotmail.fr>
2024-05-20 14:51:05 +0200

    task0, pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-""" Return the addition of two integer
->>> add_integer(5, 10)
-15
->>> add_integer(2)
-100"""
+"""
+    add two integers of float casted to integers.
+
+    Parameters:
+    a (int or float): the frist number.
+    b (int or float): the seconde number, default is 98.
+
+    Raises:
+    TypeError: If a or b are not integers or float.
+
+    Return:
+    int: The sum of a and b casted to integers.
+"""
 
 
 def add_integer(a, b=98):
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer")
+        raise TypeError("b must be an integer")
     return int(a) + int(b)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod(add_integer.txt)
