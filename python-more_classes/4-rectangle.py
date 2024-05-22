85de53f6252bbcfc4a4ce98fa9dfaaf93ecc3e15
Grilindor <marcor91@hotmail.fr>
2024-05-22 14:19:07 +0200

    task4, repr() should return a string representation of the rectangle

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """ Contains a class definition of 'Rectangle' """
 
-from typing import Any
-
 
 class Rectangle:
-    # a defined rectangle
+    """a defined rectangle"""
     def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
@@ -16,10 +22,10 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        # Check if value is an integer
+        """Check if value is an integer"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        # Check if value is greater than or equal to 0
+        """Check if value is greater than or equal to 0"""
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__width = value
@@ -30,10 +36,10 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        # Check if value is an integer
+        """Check if value is an integer"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        # Check if value is greater than or equal to 0
+        """Check if value is greater than or equal to 0"""
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
@@ -42,13 +48,13 @@ class Rectangle:
         return self.width * self.height
 
     def perimeter(self):
-        # return perimeter of rectangle
+        """return perimeter of rectangle"""
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.height + self.width)
 
-# print the rectangle with the character #
     def __str__(self):
+        """print the rectangle with the character"""
         if self.__width == 0 or self.__height == 0:
             return ""
 
@@ -57,3 +63,6 @@ class Rectangle:
                 for j in range(self.height - 1):
                     print('#' * self.__width)
                 return '#' * self.width
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
