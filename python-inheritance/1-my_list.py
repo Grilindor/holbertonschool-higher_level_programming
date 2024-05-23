cb83586a86048c91c4729f200101a8c9f21bedd1
Grilindor <marcor91@hotmail.fr>
2024-05-23 16:20:12 +0200

    pyco

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,10 +4,7 @@
 
 class MyList(list):
     """Public instance method that prints the list, but sorted"""
-<<<<<<< HEAD
 
-=======
->>>>>>> 286d024e6282b3356b7ce307ed203e5d36e8e9fc
     def print_sorted(self):
         for append in self:
             if not isinstance(append, int):
