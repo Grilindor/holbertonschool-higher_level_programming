286d024e6282b3356b7ce307ed203e5d36e8e9fc
Grilindor <marcor91@hotmail.fr>
2024-05-23 16:16:57 +0200

    task1, pycostyle

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""class MyList that inherits from list"""
+
 
 class MyList(list):
+    """Public instance method that prints the list, but sorted"""
     def print_sorted(self):
         for append in self:
             if not isinstance(append, int):
                 raise TypeError("not all elements in it are integer")
         print(sorted(self))
-
