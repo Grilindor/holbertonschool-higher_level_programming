ee7a87c840115ef201d9da5db19b36d7fae7fe49
Grilindor <marcor91@hotmail.fr>
2024-05-23 17:25:03 +0200

    task3, check if an object is an instance of a specified class or any of its subclasses

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that returns True if the object inherited from a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """return True or False"""
+    return isinstance(obj, a_class)
+
