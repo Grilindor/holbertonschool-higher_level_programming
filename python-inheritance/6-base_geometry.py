8c1fb3a6e942a1f7c2336f26125b90730b610499
Grilindor <marcor91@hotmail.fr>
2024-05-24 14:46:21 +0200

    task6, Public instance method raises an Exception with a message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class of BaseGeometry"""
+
+
+class BaseGeometry:
+    """Public instance method raises an Exception with a message"""
+    def area(self):
+        raise Exception("area() is not implemented")
