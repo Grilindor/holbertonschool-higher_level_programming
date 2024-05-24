7b32ea75b3a06de8f947ee9fb3e5d213d06361fa
Grilindor <marcor91@hotmail.fr>
2024-05-24 15:52:19 +0200

    task7, pycostyle

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,4 +12,3 @@ class BaseGeometry:
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
-
