68c66ee798b0db6b875ba22803bb3433bc0c82ab
Grilindor <marcor91@hotmail.fr>
2024-05-24 15:55:01 +0200

    task7, add test and pyco

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,6 +8,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Public instance method that validates value"""
         if not isinstance(value, int):
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
