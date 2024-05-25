907521b2a3e06830d9e603dbac68c576e961f53c
Grilindor <marcor91@hotmail.fr>
2024-05-25 16:07:58 +0200

    correcting the calcul for circle

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -16,10 +16,10 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return 2 * (math.pi * self.radius)
+        return  math.pi * self.radius ** 2
 
     def perimeter(self):
-        return math.pi*(self.radius * 2)
+        return 2 * math.pi * self.radius
 
 class Rectangle(Shape):
     def __init__(self, width, height):
