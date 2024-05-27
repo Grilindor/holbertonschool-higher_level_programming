eb6fb7e7181c2384811444bb5e08141161e5a9ee
Grilindor <marcor91@hotmail.fr>
2024-05-27 10:11:25 +0200

    task0, function that reads a text file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that reads a text file"""
+
+
+def read_file(filename=""):
+    """ reads a text file and prints it to stdout"""
+    with open(filename, encoding= "utf-8") as file0_toread:
+        print(file0_toread.read())
