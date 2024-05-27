ed97ced9a3ccd790cdf28ae10894d4749b5781bb
Grilindor <marcor91@hotmail.fr>
2024-05-27 11:21:32 +0200

    task0, remove space after =

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """ reads a text file and prints it to stdout"""
-    with open(filename, encoding= "utf-8") as file0_toread:
+    with open(filename, encoding="utf-8") as file0_toread:
         print(file0_toread.read())
