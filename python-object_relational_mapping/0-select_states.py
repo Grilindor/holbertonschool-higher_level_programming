2ef00a46beaac2bb29ae73be1fe45b906db50331
Grilindor <marcor91@hotmail.fr>
2024-06-21 09:30:43 +0200

    pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,11 @@ def list_states():
     database_name = sys.argv[3]
 
     # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", user=mysql_username, passwd=mysql_password, db=database_name, port=3306)
+    db = MySQLdb.connect(host="localhost",
+                         user=mysql_username,
+                         passwd=mysql_password,
+                         db=database_name,
+                         port=3306)
 
     # Create a cursor to interact with the database
     cursor = db.cursor()
@@ -31,6 +35,7 @@ def list_states():
     # Close the cursor and the connection
     cursor.close()
     db.close()
+
 
 # Ensure the script is not executed when imported
 if __name__ == "__main__":
