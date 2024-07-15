13a8ceee6f2b483a3b28d59f39738dc1640ff8d5
Grilindor <marcor91@hotmail.fr>
2024-07-15 19:34:13 +0200

    task3, Displaying Data from JSON or CSV Files in Flask

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request
+import json
+import csv
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+   return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    with open('items.json', 'r') as file:
+        data = json.load(file)
+    return render_template('items.html', items=data.get('items', []))
+
+
+def read_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+def read_csv(file_path):
+    products = []
+    with open(file_path, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            products.append({
+                "id": int(row["id"]),
+                "name": row["name"],
+                "category": row["category"],
+                "price": float(row["price"])
+            })
+    return products
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    product_id = request.args.get('id', type=int)
+
+    if source == 'json':
+        products = read_json('products.json')
+    elif source == 'csv':
+        products = read_csv('products.csv')
+    else:
+        return render_template('product_display.html', error="Wrong source")
+
+    if product_id is not None:
+        products = [product for product in products if product['id'] == product_id]
+        if not products:
+            return render_template('product_display.html', error="Product not found")
+
+    return render_template('product_display.html', products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True)
